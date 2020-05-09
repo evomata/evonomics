8e4ceedba2b86fcc0bd00269ce200a60913bbd17
Geordon Worley <vadixidav@gmail.com>
2020-05-09 19:39:23 -0400

    get rid of jumping and have only conditional instructions

--- src/sim/brain.rs
+++ src/sim/brain.rs
@@ -59,14 +59,7 @@ impl Dna {
         if rng.gen_bool(0.5) {
             // Add a codon.
             let position = rng.gen_range(0, self.sequence.len() + 1);
-            let newseqlen = self.sequence.len() + 1;
-            // Fix any branching codons affected.
-            // Must be done before new one is added.
-            for codon in &mut self.sequence {
-                codon.shift_genes(newseqlen, position, 1);
-            }
-            self.sequence
-                .insert(position, rng.gen::<Codon>().sanitize(newseqlen, NUM_STATE));
+            self.sequence.insert(position, rng.gen::<Codon>());
             // Move entries.
             for entry in &mut self.entries {
                 if *entry >= position {
@@ -84,11 +77,6 @@ impl Dna {
                 if *entry > position {
                     *entry -= 1;
                 }
-            }
-            // Fix any branching codons affected.
-            let seqlen = self.sequence.len();
-            for codon in &mut self.sequence {
-                codon.shift_genes(seqlen, position, -1);
             }
         }
 
@@ -138,20 +126,15 @@ impl Dna {
                     }
                 }
                 Codon::Literal(n) => stack.push(n),
-                Codon::Less(t, f) => {
+                Codon::Less => {
                     if let Some(o) = stack.pop().and_then(|b| stack.pop().map(|a| a < b)) {
-                        let dest = if o { t } else { f };
-                        at = dest as usize;
-                        // Do not follow through to the at incrementer.
-                        continue;
+                        // Skip the next instruction if its false. It is only executed if true.
+                        if !o {
+                            at = (at + 1) % self.sequence.len();
+                        }
                     } else {
                         break;
                     }
-                }
-                Codon::Jump(dest) => {
-                    at = dest as usize;
-                    // Do not follow through to the at incrementer.
-                    continue;
                 }
                 Codon::Copy(pos) => {
                     if stack.is_empty() || pos > stack.len() as u32 {
@@ -182,11 +165,7 @@ impl Dna {
 impl Distribution<Dna> for Standard {
     fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Dna {
         let sequence_len = (rng.sample::<f64, _>(Exp1) * INITIAL_GENOME_SCALE) as usize;
-        let sequence = rng
-            .sample_iter(Standard)
-            .take(sequence_len)
-            .map(|c: Codon| c.sanitize(sequence_len, NUM_STATE))
-            .collect();
+        let sequence = rng.sample_iter(Standard).take(sequence_len).collect();
         let entries = {
             if sequence_len == 0 {
                 vec![]
@@ -208,8 +187,7 @@ enum Codon {
     Mul,
     Div,
     Literal(f64),
-    Less(u32, u32),
-    Jump(u32),
+    Less,
     Copy(u32),
     Read(u32),
     Input(u32),
@@ -219,73 +197,34 @@ enum Codon {
     Nothing,
 }
 
-impl Codon {
-    /// Puts all memory accesses and jumps in bounds except for input or output codons.
-    fn sanitize(self, genes: usize, memory: usize) -> Self {
-        let san_branch = |v| v % genes as u32;
-        let san_mem = |v| v % memory as u32;
-        match self {
-            Codon::Less(a, b) => Codon::Less(san_branch(a), san_branch(b)),
-            Codon::Jump(a) => Codon::Jump(san_branch(a)),
-            Codon::Read(a) => Codon::Read(san_mem(a)),
-            Codon::Write(a) => Codon::Write(san_mem(a)),
-            c => c,
-        }
-    }
-
-    /// Passed the gene position that was inserted/removed and a direction that is positive if inserted or negative if removed.
-    /// Also passed the number of genes after shifting.
-    fn shift_genes(&mut self, genes: usize, shiftpos: usize, shiftdir: isize) {
-        let shift_branch = |v| {
-            if v > shiftpos as u32 {
-                (v as i32 + shiftdir as i32) as u32
-            } else if v < shiftpos as u32 {
-                v
-            } else if shiftdir > 0 {
-                // If its positive, it wont get destroyed.
-                (v as i32 + shiftdir as i32) as u32
-            } else {
-                // The destination got destroyed, so generate a new destination.
-                rand::thread_rng().gen::<u32>() % genes as u32
-            }
-        };
-        *self = match *self {
-            Codon::Less(a, b) => Codon::Less(shift_branch(a), shift_branch(b)),
-            Codon::Jump(a) => Codon::Jump(shift_branch(a)),
-            c => c,
-        };
-    }
-}
-
 impl Distribution<Codon> for Standard {
     fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Codon {
-        match rng.gen_range(0, 14) {
+        match rng.gen_range(0, 13) {
             0 => Codon::Add,
             1 => Codon::Sub,
             2 => Codon::Mul,
             3 => Codon::Div,
             4 => Codon::Literal(rng.gen()),
-            5 => Codon::Less(rng.gen(), rng.gen()),
-            6 => Codon::Jump(rng.gen()),
-            7 => Codon::Copy(rng.gen()),
-            8 => Codon::Read(rng.gen()),
-            9 => Codon::Input(rng.gen()),
-            10 => Codon::Write(rng.gen()),
-            11 => Codon::Move(match rng.gen_range(0, 4) {
+            5 => Codon::Less,
+            6 => Codon::Copy(rng.gen()),
+            7 => Codon::Read(rng.gen::<u32>() % NUM_STATE as u32),
+            8 => Codon::Input(rng.gen()),
+            9 => Codon::Write(rng.gen::<u32>() % NUM_STATE as u32),
+            10 => Codon::Move(match rng.gen_range(0, 4) {
                 0 => MooreDirection::Right,
                 1 => MooreDirection::Up,
                 2 => MooreDirection::Left,
                 3 => MooreDirection::Down,
                 _ => unreachable!(),
             }),
-            12 => Codon::Divide(match rng.gen_range(0, 4) {
+            11 => Codon::Divide(match rng.gen_range(0, 4) {
                 0 => MooreDirection::Right,
                 1 => MooreDirection::Up,
                 2 => MooreDirection::Left,
                 3 => MooreDirection::Down,
                 _ => unreachable!(),
             }),
-            13 => Codon::Nothing,
+            12 => Codon::Nothing,
             _ => unreachable!(),
         }
     }
