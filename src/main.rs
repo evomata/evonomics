044c5ac7fbab4099b878afd82537e15f13ece728
Gage Ervin <gage.william.ervin@gmail.com>
2020-05-15 23:51:47 -0600

    moved tick count dispaly

--- src/main.rs
+++ src/main.rs
@@ -125,7 +125,7 @@ fn spawn_rate(
     spawn_rate: f32,
 ) -> f64 {
     if is_inverse_rate_type {
-        spawn_rate as f64 / ( (cell_count+1) * height ) as f64
+        spawn_rate as f64 / ((cell_count + 1) * height) as f64
     } else {
         (SPAWN_CURVE.powf(1.0 - spawn_rate as f64) - SPAWN_CURVE) / (1.0 - SPAWN_CURVE)
     }
@@ -291,8 +291,10 @@ impl<'a> Application for EvonomicsWorld {
                     Some(ref mut tx) => {
                         // If the channel is full, dont send it.
                         match tx.try_send(sim::ToSim::Tick(self.speed)).ok() {
-                            Some(_) => { self.total_tick_count += self.speed as u64; },
-                            None => {},
+                            Some(_) => {
+                                self.total_tick_count += self.speed as u64;
+                            }
+                            None => {}
                         }
                         self.update(Message::SpawnRateChanged(self.spawn_rate));
                     }
@@ -465,6 +467,13 @@ impl<'a> Application for EvonomicsWorld {
                             .vertical_alignment(VerticalAlignment::Bottom)
                             .horizontal_alignment(HorizontalAlignment::Center)
                             .width(Length::Fill),
+                        )
+                        .push(
+                            Text::new(format!("Total Ticks: {}", self.total_tick_count))
+                                .size(16)
+                                .vertical_alignment(VerticalAlignment::Bottom)
+                                .horizontal_alignment(HorizontalAlignment::Center)
+                                .width(Length::Fill),
                         ),
                 )
                 .style(style::Theme::Nested);
@@ -532,7 +541,6 @@ impl<'a> Application for EvonomicsWorld {
                         .on_press(Message::ToggleSim),
                     )
                     .push(fps_controls)
-                    .push( Text::new( format!( "Total Ticks: {}", self.total_tick_count ) ) )
                     .push(spawn_controls)
                     .push(
                         Button::new(
