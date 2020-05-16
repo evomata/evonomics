f51ec994f5e79056e57772f8bb507aa45455f592
Geordon Worley <vadixidav@gmail.com>
2020-05-16 19:34:13 -0400

    add more padding on bottom of graphs

--- src/plot.rs
+++ src/plot.rs
@@ -23,6 +23,7 @@ pub fn graph_bids_asks(
 
     let mut chart = ChartBuilder::on(&root)
         .set_label_area_size(LabelAreaPosition::Left, 30)
+        .set_label_area_size(LabelAreaPosition::Bottom, 5)
         .build_ranged(0..bids.len(), min..max)?
         .set_secondary_coord(0..bids.len(), min..max);
 
@@ -58,6 +59,7 @@ pub fn graph_reserve(reserves: &[u32]) -> Result<image::Handle, Box<dyn std::err
 
     let mut chart = ChartBuilder::on(&root)
         .set_label_area_size(LabelAreaPosition::Left, 40)
+        .set_label_area_size(LabelAreaPosition::Bottom, 5)
         .build_ranged(0..reserves.len(), min..max)?;
 
     chart
